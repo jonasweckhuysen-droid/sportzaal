86397079fc0948daf66fb7057a1a5bde6ffb1352
jonasweckhuysen-droid <jonasweckhuysen@gmail.com>
2026-06-16 16:36:09 +0200

    Update cache paths in service worker

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,10 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js"
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js"
       ]);
     })
   );
